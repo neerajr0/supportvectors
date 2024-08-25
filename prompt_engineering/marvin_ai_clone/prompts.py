291953a093edd4e35c54a67801a98ede8f2b9099
Neeraj Ramachandran <ramachandraneeraj@gmail.com>
2024-08-25 12:15:25 -0500

    refine prompts to use costar

--- prompt_engineering/marvin_ai_clone/prompts.py
+++ prompt_engineering/marvin_ai_clone/prompts.py
@@ -1,26 +1,42 @@
+
+SYSTEM_PROMPT = "You are emulating the functionality of the AI Engineering toolkit Marvin AI. \
+    You will respond to user prompts only with the response, without any extra language, like an API would. \
+    If any output schema is provided for the response, be sure to follow that schema."
+
 CLASSIFY_MESSAGES = [
-    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "system", "content": SYSTEM_PROMPT},
     {
         "role": "user",
-        "content": " Given text and a list of labels, return the label \
+        "content": "# OBJECTIVE # \
+            Given text and a list of labels, return the label \
             that most closely classifies the text. \
-            e.g. text = 'Marvin is so easy to use!' and \
-            labels=['positive', 'negative'] would return \
-            'positive'. Please only return the response to the query without any extra language."
+            # EXAMPLE # \
+            text = 'Marvin is so easy to use!' and \
+            labels=['positive', 'negative'] \
+            would return 'positive'. \
+            # AUDIENCE # \
+            Imagine that developers are using you as an API to get structured responses. \
+            # RESPONSE # \
+            ONLY the answer to the query formatted according to the provided schema. \
+            Do not create new \
+            labels other than the ones provided. \
+            Do not return None or no match, always choose the label that \
+            most closely classifies the text."
     }
 ]
 
 EXTRACT_MESSAGES = [
-    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "system", "content": SYSTEM_PROMPT},
     {
         "role": "user",
-        "content": "Given text and a Pydantic model serialized with json, \
+         "content": "# OBJECTIVE # \
+            Given text and a Pydantic model serialized with json, \
             return a list of instances of this model reflecting \
             instances of that model present in the text. \
-            e.g. given model = \
-            {'properties': {'city': {'title': 'City', 'type': 'string'}, 'state': {'title': 'State', 'type': 'string'}}, 'required': ['city', 'state'], 'title': 'Location', 'type': 'object'} \
-            and text = 'I moved from NY to CHI' \
-            the result would be \
+            # EXAMPLE # \
+            model = {'properties': {'city': {'title': 'City', 'type': 'string'}, 'state': {'title': 'State', 'type': 'string'}}, 'required': ['city', 'state'], 'title': 'Location', 'type': 'object'} \
+            and text = = 'I moved from NY to CHI' \
+            would return \
             [ \
                 Location(city='New York', state='New York'), \
                 Location(city='Chicago', state='Illinois') \
@@ -29,21 +45,27 @@ EXTRACT_MESSAGES = [
             that meet the requirements of the instructions. For example, if in the \
             above example, if the instructions are 'locations on the east coast', you \
             would only return the model for New York. \
-            Please only return the response to the query without any extra language."
+            # AUDIENCE # \
+            Imagine that developers are using you as an API to get structured responses. \
+            # RESPONSE # \
+            ONLY the answer to the query formatted according to the provided schema."
     }
 ]
 
 GENERATE_MESSAGES = [
-    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "system", "content": SYSTEM_PROMPT},
     {
         "role": "user",
-        "content": "Given a Pydantic model, a text instruction \
+         "content": "# OBJECTIVE # \
+            Given a Pydantic model, a text instruction \
             and an integer n representing the number \
             of examples to generate \
             return a list of length n of instances of the model \
             where each instance represents an example of the \
             instruction provided. \
-            e.g. given n = 4, instruction = 'cities in the United States named after presidents' \
+            # EXAMPLE # \
+            n = 4 \
+            instruction = 'cities in the United States named after presidents' \
             and model = \
             {'properties': {'city': {'title': 'City', 'type': 'string'}, 'state': {'title': 'State', 'type': 'string'}}, 'required': ['city', 'state'], 'title': 'Location', 'type': 'object'} \
             the result would be \
@@ -53,28 +75,52 @@ GENERATE_MESSAGES = [
                 Location(city='Cleveland', state='Ohio'), \
                 Location(city='Lincoln', state='Nebraska'), \
             ] \
-            Please only return the response to the query without any extra language."
+            # AUDIENCE # \
+            Imagine that developers are using you as an API to get structured responses. \
+            # RESPONSE # \
+            ONLY the answer to the query formatted according to the provided schema."
     }
 ]
 
 DEFINE_MESSAGES = [
-    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "system", "content": SYSTEM_PROMPT},
     {
         "role": "user",
-        "content": "Given a term, provide a dictionary-style \
+        "content": "# OBJECTIVE # \
+            Given a term, provide a dictionary-style \
             definition of the term, including part(s) of speech \
             and definition(s) - (Note a term can have multiple \
             definitions or parts of speech). \
-            Please only return the response to the query without any extra language."
-        },
+            # EXAMPLE # \
+            given the term 'contract', you would return the following: \
+            **contract** \
+            *noun*  \
+            1. A written or spoken agreement, especially one concerning employment, sales, or tenancy, that is intended to be enforceable by law. \
+            2. A formal agreement between two or more parties, creating obligations that are enforceable or otherwise recognizable by law. \
+            3. In business, a document that outlines terms, conditions, and obligations of parties involved in a transaction. \
+            *verb* \
+            1. To enter into a formal agreement with another party. \
+            2. To decrease in size, number, or range; to shrink. \
+            3. To acquire or develop (a disease, debt, etc.). \
+            # AUDIENCE # \
+            Imagine that developers are using you as an API to get structured responses. \
+            # RESPONSE # \
+            ONLY the answer to the query formatted according to the provided schema."
+    }
 ]
 
 SPELL_CHECK_MESSAGES = [
-    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "system", "content": SYSTEM_PROMPT},
     {
         "role": "user",
-        "content": "Given some text, please correct any spelling and grammar errors and return the fixed result. \
-            e.g. 'i ate wafles; pankakes; and sirup for brekfist' would become 'I ate waffles, pancakes and syrup for breakfast. \
-            Please only return the response to the query without any extra language."
+        "content": "# OBJECTIVE # \
+            Given some text, please correct any spelling and grammar errors and return the fixed result. \
+            # EXAMPLE # \
+            Given the text 'i ate wafles; pankakes; and sirup for brekfist', you would return \
+            'I ate waffles, pancakes and syrup for breakfast.' \
+            # AUDIENCE # \
+            Imagine that developers are using you as an API to get structured responses. \
+            # RESPONSE # \
+            ONLY the answer to the query formatted according to the provided schema."
     },
 ]
